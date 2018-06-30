46bf25a819fdbaf630c10ee82627efbe8c67d0bb
aixiu <4815563@qq.com>
2018-07-01 00:53:14 +0800

    引用外部模块备注

--- ls-40.py
+++ ls-40.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import time, subprocess
+import time, subprocess   # 引用外部程序模块  subprocess
 
 timeLeft = 5
 while timeLeft > 0:
@@ -12,7 +12,7 @@ while timeLeft > 0:
 print('Music, Now!')
 
 try:
-    subprocess.run(['start', 'f4 - 流星雨.mp3'])
+    subprocess.run(['start', 'f4 - 流星雨.mp3'])   # 用subprocess 打开外部程序
 except FileNotFoundError: 
     print('\n{:*^20}'.format('找不到播放文件'))
 
