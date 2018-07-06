3f59979d574600c9a91b3aa2b7f5fb07f86b60b7
aixiu <4815563@qq.com>
2018-07-06 21:05:17 +0800

    update ls58

---
+++ ls-58-duixiang.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#剑客
+class Warriors:
+
+    def __init__(self, name, Kungfu, attkMax, DefendMax):
+        self.name = name
+        self.energyValue = Kungfu
+        
