e21176c9848b3baa217644966fa25f82a4f220ad
aixiu <4815563@qq.com>
2018-07-31 21:41:51 +0800

    新增雪锋文件夹

---
+++ liao_xuefeng/test.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+def enroll(name, gender, age=6, city='Beijing'):
+    print('name:', name)
+    print('gender:', gender)
+    print('age:', age)
+    print('city:', city)
+
+enroll('Sarah', 'F')
