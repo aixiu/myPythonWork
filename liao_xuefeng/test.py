fff9f137180058e105cc1698cc47f849f320a0f1
aixiu <4815563@qq.com>
2018-08-02 22:08:10 +0800

    语法糖

--- liao_xuefeng/test.py
+++ liao_xuefeng/test.py
@@ -2,10 +2,74 @@
 # -*- coding: utf-8 -*-
 
 
-def enroll(name, gender, age=6, city='Beijing'):
-    print('name:', name)
-    print('gender:', gender)
-    print('age:', age)
-    print('city:', city)
+# L1 = ['Hello', 'World', 18, 'Apple', None]
+# L2 = [s.lower() for s in L1 if isinstance(s, str)]
 
-enroll('Sarah', 'F')
+# print(L2)
+# if L2 == ['hello', 'world', 'apple']:
+#     print('测试通过!')
+# else:
+#     print('测试失败!')
+
+# ----------------------------------------------
+
+# def fib(max):
+#     n, a, b = 0, 0, 1
+#     while n < max:
+#         print(b)
+#         a, b = b, a + b
+#         n = n + 1
+#     return 'done'
+
+# fib(6)
+
+# ------------------------------------
+
+# def fib(max):
+#     n, a, b = 0, 0, 1
+#     while n < max:
+#         yield b
+#         a, b = b, a + b
+#         n = n + 1
+#     return 'done'
+
+# for i in fib(6):
+#     print(i)
+
+# ---------------------------------------------
+
+# l = list(filter(lambda n: n % 2 == 0, range(1, 20)))
+# print(l)
+
+# import time 
+
+# def test():
+#     time.sleep(2)
+#     print("test is running!")
+
+# def deco(func):  
+#     start = time.time()
+#     func() #2
+#     stop = time.time()
+#     print(stop-start)
+
+# deco(test) #1
+
+
+import time
+
+def timer(func): #5
+    def deco():  
+        start = time.time()
+        func()
+        stop = time.time()
+        print(stop-start)
+    return deco
+@timer
+def test():
+    time.sleep(2)
+    print("test is running!") 
+
+
+
+test() #7
