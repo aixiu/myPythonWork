1cd405c9e294ca241df98334d951dff5ee3c7f63
aixiu <4815563@qq.com>
2018-08-08 22:03:43 +0800

    update

--- OldBoy/装饰器.py
+++ OldBoy/装饰器.py
@@ -7,14 +7,20 @@ def login(func):
         return func(namehaha)
     return inner
 
-# @login
+@login
 def home(name):
-    print('Welcome {} to Home page'.format(name))
+    print('Welcome [{}] to Home page'.format(name))
     
 # def tv(name):
 #     print('Welcome {} to TV page'.format(name))
 
 # def moive(name):
 #     print('Welcome {} to Movie page'.format(name))
-home = login(home)
+# home = login(home)
 home('aixiu')
+s = home.__name__
+print(s)
+
+# 调用新的home()相当于调用inner()，所以inner要接收home传过来的实参'aixiu'，
+# inner必须要有一个形参'namehaha'（形参名字可以自己定义），接下来，func相当于home,
+# 也必须有参数，且形参名字和inner形参一至。
