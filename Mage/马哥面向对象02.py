b0fd4c5a7c2b179f6649d8415ee47a4d8e032c83
aixiu <4815563@qq.com>
2018-11-26 20:48:32 +0800

    update

--- Mage/马哥面向对象02.py
+++ Mage/马哥面向对象02.py
@@ -19,7 +19,7 @@ class Person(object):
     def __init__(self, name, age = 18):
         self.name = name
         self.y = age
-        # 注意，__init__()  方法不能有返回值，也就是只能是None 
+        # 注意，__init__()  方法不能有返回值，也就是只能是None 不要出现 return 关键字
 
     def show(self):
         print(self.name, self.y)
@@ -33,3 +33,6 @@ print(a.x, b.x)
 print(a.y, b.y)
 
 a.show()
+
+print(a.__dict__)
+print(sorted(a.__dict__.items()), end='\n')
