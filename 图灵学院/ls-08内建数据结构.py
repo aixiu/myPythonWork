5c3c858e872463261c5f248dd94be4b03d92b626
aixiu <4815563@qq.com>
2019-03-09 20:13:37 +0800

    08课时增加内容

--- 图灵学院/ls-08内建数据结构.py
+++ 图灵学院/ls-08内建数据结构.py
@@ -29,3 +29,37 @@ def hano(n, a, b, c):
     b：代表第二个塔，中间过渡的塔
     c：代表第三个塔, 目标塔
     '''
+    if n == 1:
+        print(a, '-->', c)
+        return None
+
+    if n == 2:
+        print(a, '-->', b)
+        print(a, '-->', c)
+        print(b, '-->', c)
+        return None
+
+    # 把n-1个盘子，从a塔借助于c塔，挪到b塔上去
+    hano(n-1, a, c, b)
+    print(a, "-->", c)
+    # 把n-1个盘子，从b塔，借助于a塔，挪到c塔上去
+    hano(n-1, b, a, c)
+
+a = "A"
+b = "B"
+c = "C"
+
+n = 10
+hano(n, a, b, c)
+
+
+# 第二种  利用递归函数移动汉诺塔:
+def move(n, a, b, c):
+    if n == 1:
+        print('move', a, '-->', c)
+    else:
+        move(n-1, a, c, b)
+        move(1, a, b, c)
+        move(n-1, b, a, c)
+
+move(4, 'A', 'B', 'C')
