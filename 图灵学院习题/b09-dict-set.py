5a60d001d2ce04fe408ea8ebe73a4571db358ded
aixiu <4815563@qq.com>
2019-05-17 21:45:03 +0800

    add bo09

---
+++ 图灵学院习题/b09-dict-set.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# 写一个程序，来管理用于登陆系统的用户信息：登录名和密码。
+# 登录用户帐号建立后，已存在用户可以用登录名和密码重返系统，新用户不能用别人的用户名
+# 建立用户帐号
+
+user_pass = {'laotie':'password', 'huniu':'admin'}
