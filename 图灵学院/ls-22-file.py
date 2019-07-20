b416dd9353c83a677e940b7013a42ddf19a8f005
aixiu <4815563@qq.com>
2019-07-20 15:35:21 +0800

    update ls-22

--- 图灵学院/ls-22-file.py
+++ 图灵学院/ls-22-file.py
@@ -140,3 +140,185 @@ with open(r'test01.txt', 'r', encoding='utf-8') as f:
     # 区别：
         # write函数参数只能是字符串
         # writerline参数可以是字符串，也可以是字符序列
+
+    
+# write 案例
+
+# 1. 向文件追加一句诗
+# a代表追加方式打开
+
+with open(r'test01.txt', 'a',encoding='utf-8') as f:
+    f.write('\n生活不仅有眼前的苟且\n还有远方的苟且')
+
+# 可以直接写入行，用 writeline
+# writelines表示写入很多行，参数可以是list格式
+
+with open(r'test01.txt', 'a',encoding='utf-8') as f:
+    f.writelines('\n还有远方的思念')
+    f.writelines('\n等等')
+
+l = ['I', 'love', 'wangxiaojing']
+with open(r'test01.txt', 'a',encoding='utf-8') as f:
+    f.writelines(l)
+
+
+
+# 持久化 - pickle
+    # 序列化（持久化，落地）：把程序运行中的信息保存在磁盘上
+    # 反序列化： 序列号的逆过程
+    # pickle： python提供的序列化模块
+    # pickle.dump:序列化
+    # pickle.load:反序列化
+
+
+# 序列化案例
+import pickle
+
+age = 19
+with open(r'test01.txt', 'wb') as f:
+    pickle.dump(age, f)
+
+# 反序列化案例
+
+import pickle
+
+with open(r'test01.txt', 'rb') as f:
+    age = pickle.load(f)
+    print(age)
+
+
+# 序列化案例2
+
+import pickle
+a = [19, 'shendl', 'HOHO', [200, 50]]
+with open(r'test01.txt', 'wb') as f:
+    pickle.dump(a, f)
+
+with open(r'test01.txt', 'rb') as f:
+    a = pickle.load(f)
+
+    print(a)
+
+
+# 持久化-shelve
+    # 持久化工具
+    # 类似字典，用kv对保存数据，存取方式跟字典也类似
+    # open, close
+
+
+# 使用 shelve 创建文件并使用
+
+import shelve
+
+# 打开文件
+# shv相当于一个字典
+shv = shelve.open(r'shv.db')
+shv['one'] = 1
+shv['two'] = 2
+shv['three'] = 3
+
+shv.close()
+
+# 通过以上案例发现，shelve自动创建的不仅仅是一个shv.db文件，
+# 还包括其他格式文件
+
+# shelve 读取案例
+
+shv = shelve.open(r'shv.db')
+try:
+    print(shv['one'])
+    print(shv['two'])
+    print(shv['three'])
+except Exception as e:
+    print('还错么')
+finally:
+    shv.close()
+
+
+# shelve特性
+    # 不支持多个应用并行写入
+        # 为了解决这个问题，open的时候可以使用flag=r
+    # 写回问题
+        # shelve情况下不会等待持久化对象进行任何修改
+        # 解决方法： 强制写回：writeback=True
+
+# shelve 之只读打开
+import shelve
+
+shv = shelve.open(r'shv.db', flag='r')
+
+try:
+    k1 = shv['one']
+    print(k1)
+finally:
+    shv.close()
+
+
+
+
+import shelve 
+
+shv = shelve.open(r'shv.db')
+try:
+    shv['one'] = {"eins":1, "zwei":2, "drei":3}
+finally:
+    shv.close()
+
+
+shv = shelve.open(r'shv.db')
+try:
+    one = shv['one']
+    print(one)
+finally:
+    shv.close()
+
+
+# shelve忘记写回，需要使用强制写回
+shv = shelve.open(r'shv.db')
+try:
+    k1 = shv['one']
+    print(k1)
+    # 此时，一旦shelve关闭，则内容还是存在于内存中，没有写回数据库
+    k1["eins"] =100
+finally:
+    shv.close()
+    
+    
+shv = shelve.open(r'shv.db')
+try:
+    k1 = shv['one']
+    print(k1)
+finally:
+    shv.close()
+
+
+
+# shelve忘记写回，需要使用强制写回 writeback=True
+shv = shelve.open(r'shv.db', writeback=True)
+try:
+    k1 = shv['one']
+    print(k1)
+    # 此时，一旦shelve关闭，则内容还是存在于内存中，没有写回数据库
+    k1["eins"] =100
+finally:
+    shv.close()
+    
+    
+shv = shelve.open(r'shv.db')
+try:
+    k1 = shv['one']
+    print(k1)
+finally:
+    shv.close()
+
+
+# shelve 使用 with 管理上下文环境
+
+with shelve.open(r'shv.db', writeback=True) as shv:
+    k1 = shv['one']
+    print(k1)
+    # 此时，一旦shelve关闭，则内容写回数据库
+    k1['eins'] = 1000
+
+with shelve.open(r'shv.db') as shv:
+    print(shv['one'])
