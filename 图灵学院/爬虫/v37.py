b32c04e8fd9312069df437905687bedbedfd2164
aixiu <4815563@qq.com>
2019-08-07 21:58:20 +0800

    更新爬虫

--- 图灵学院/爬虫/v37.py
+++ 图灵学院/爬虫/v37.py
@@ -5,12 +5,12 @@
 
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
+import time
 
 chrome_options = Options()
-# chrome_options.add_argument('--headless')
 chrome_options.add_argument('--disable-gpu')
-# chrome_options.add_argument("window-size=1024,768")
-chrome_options.add_argument("--no-sandbox")
+# chrome_options.add_argument("--no-sandbox")
 chrome_options.binary_location = r"D:\\Program Files\\Chrome\\Chrome\\chrome.exe"
 
 # 添加浏览器实例，手动添加路径
@@ -21,6 +21,46 @@ url = 'http://www.baidu.com'
 dirver.get(url)
 
 text = dirver.find_element_by_id('wrapper').text
-
 print(text)
 
+print(dirver.title)
+
+# 获取页面的快照
+dirver.save_screenshot('index.png')
+
+# id='kw'的，是百度的输入框，得到输入框的UI元素后直接输入‘大熊猫’  .send_keys（）是输入的意思
+dirver.find_element_by_id('kw').send_keys(u'大熊猫')
+
+# 点击查找按钮  # id='su' 是百度搜索的按钮， click模拟点击
+
+dirver.find_element_by_id('su').click()
+
+time.sleep(5)
+dirver.save_screenshot('daxiongma.png')
+
+
+# 获取当前页面的cookie
+print(dirver.get_cookies())
+
+
+# 模拟输入两个按键 ctrl+a 
+dirver.find_element_by_id('kw').send_keys(Keys.CONTROL, 'a')
+
+# ctrl+x 是剪切键
+dirver.find_element_by_id('kw').send_keys(Keys.CONTROL, 'x')
+
+dirver.find_element_by_id('kw').send_keys(u'航空母舰')
+dirver.save_screenshot('hangmu.png')
+
+# 点击查找按钮  # id='su' 是百度搜索的按钮， click模拟点击
+
+dirver.find_element_by_id('su').send_keys(Keys.RETURN)
+time.sleep(5)
+dirver.save_screenshot('hangmu2.png')
+
+# 清空输入框
+dirver.find_element_by_id('kw').clear()
+dirver.save_screenshot('clear.png')
+
+# 关闭浏览器
+dirver.quit()
