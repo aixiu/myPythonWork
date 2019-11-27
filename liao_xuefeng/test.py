c92a731e84d5b1afd5c127c7732e8597bd029fe1
aixiu <4815563@qq.com>
2019-11-27 19:03:32 +0800

    update

--- liao_xuefeng/test.py
+++ liao_xuefeng/test.py
@@ -73,3 +73,14 @@ def test():
 
 
 test() #7
+
+
+def fib(max):
+    n, a, b = 0, 0, 1
+    while n < max:
+        print(b)
+        a, b = b, a + b
+        n = n + 1
+    return 'done'
+
+fib(10)
